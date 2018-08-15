7e0034056018109a55fd0f366c6e5665b64c60be
Kai Xia <xiaket@gmail.com>
2018-08-15 23:07:42 +1000

    use svg over html canvas.
    Signed-off-by: Kai Xia <xiaket@gmail.com>

--- media/blog.js
+++ media/blog.js
@@ -34,27 +34,71 @@ function coarseLine(context, startx, starty, endx, endy, segments, width, coarse
   context.stroke();
   context.closePath();
 }
-function drawLogo() {
-  // 初始化
-  var canvas = document.getElementById('logo');
-  var context = canvas.getContext('2d');
-  context.globalAlpha=0.0;
-  context.fillRect(0, 0, 100, 100);
-  context.globalAlpha=1.0;
 
-  gradient = context.createRadialGradient(50,50,0,50,50,42);
-  gradient.addColorStop(0, "#4467AD");
-  gradient.addColorStop(1, "#203D76");
+function coarse_line(startx, starty, endx, endy, coarseness, segments = 2){
+  // 画一条比较粗糙的线条, 具有类似手绘的感觉.
+  // coarseness是曲线的不规则程度, segments是用几段曲线来画.
+  var x, y, choice, nextx, nexty, dx1, dx2, dy1, dy2, l, lastx, lasty, desc;
+  var desc = `M ${startx} ${starty}, `;
 
-  context.beginPath();
-  context.arc(50, 50, 42, 0, Math.PI*2, false);
-  context.fillStyle = gradient;
-  context.moveTo(100, 50);
-  context.fill()
-  context.closePath();
-  coarseLine(context, 29, 21, 29, 79, 2, 3, 1, "white");
-  coarseLine(context, 29, 57, 70, 21, 2, 3, 5, "white");
-  coarseLine(context, 45, 45, 71, 79, 2, 3, 3, "white");
+  l = Math.sqrt((startx - endx)*(startx - endx) + (starty - endy)*(starty - endy))
+
+  lastx = startx;
+  lasty = starty;
+  for(index=0; index < segments; index++){
+    choice=Math.random();
+    if (choice > 0.5){
+      delta1=Math.floor(Math.random()*(2*coarseness+1)) - coarseness;
+      delta2=-Math.floor(Math.random()*(2*coarseness+1)) + (coarseness);
+    }else{
+      delta1=-Math.floor(Math.random()*(2*coarseness+1)) + (coarseness);
+      delta2=Math.floor(Math.random()*(2*coarseness+1)) - (coarseness);
+    }
+    dx1 = Math.round((endy-starty)*delta1/l);
+    dx2 = Math.round((endy-starty)*delta2/l);
+    dy1 = Math.round((endx-startx)*delta1/l);
+    dy2 = Math.round((endx-startx)*delta2/l);
+    nextx = Math.round((endx - startx) / segments) + lastx;
+    nexty = Math.round((endy - starty) / segments) + lasty;
+    desc += ` C${(2*lastx+nextx)/3+dx1} ${(2*lasty+nexty)/3+dy1}, ${(lastx+2*nextx)/3+dx2} ${(lasty+2*nexty)/3+dy2}, ${nextx} ${nexty}`;
+    lastx = nextx;
+    lasty = nexty;
+  }
+  return desc
+}
+
+function writeK(){
+  var svg_size = document.getElementById('logo-svg').clientWidth;
+  var half = svg_size * 0.5;
+  var width = svg_size * 0.02;
+  var radius = svg_size * 0.42;
+
+  var path1_x = svg_size * 0.29;
+  var path1_y = svg_size * 0.21;
+  var path1_x_end = svg_size * 0.29;
+  var path1_y_end = svg_size * 0.79;
+
+  var path2_x = svg_size * 0.29;
+  var path2_y = svg_size * 0.57;
+  var path2_x_end = svg_size * 0.70;
+  var path2_y_end = svg_size * 0.21;
+
+  var path3_x = svg_size * 0.44;
+  var path3_y = svg_size * 0.44;
+  var path3_x_end = svg_size * 0.71;
+  var path3_y_end = svg_size * 0.79;
+
+  document.getElementById("svg-circle").setAttribute("cx", half);
+  document.getElementById("svg-circle").setAttribute("cy", half);
+  document.getElementById("svg-circle").setAttribute("r", radius);
+
+  document.getElementById("svg-path1").setAttribute("stroke-width", width);
+  document.getElementById("svg-path2").setAttribute("stroke-width", width);
+  document.getElementById("svg-path3").setAttribute("stroke-width", width);
+
+  document.getElementById("svg-path1").setAttribute("d", coarse_line(path1_x, path1_y, path1_x_end, path1_y_end, 1));
+  document.getElementById("svg-path2").setAttribute("d", coarse_line(path2_x, path2_y, path2_x_end, path2_y_end, 5));
+  document.getElementById("svg-path3").setAttribute("d", coarse_line(path3_x, path3_y, path3_x_end, path3_y_end, 3));
 }
 
 //       __
@@ -89,4 +133,3 @@ function draw_404() {
 function r(x, dx) {
   return x + Math.floor((Math.random() * dx * 2) - dx);
 }
-
